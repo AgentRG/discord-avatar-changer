a283029ffe9bee85564020b756ec92b899880634
Rostyslav Gordin <ron.gordin@gmail.com>
2018-04-03 20:31:34 -0700

    Added the choice to either randomly pick a avatar, or incrementally.
    Also general cleanup.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,43 +1,32 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import java.io.File;
-import java.util.Random;
 
 public class Main {
 
     /**
      * This project uses Selenium to upload random avatars from a specified folder to VoIP software Discord from the web.
      * If the project is built into a jar, crontab can be used to launch the jar at a specified interval.
+     *
+     * The project has two modes.
+     * 1. Randomly pick a picture from the folder and upload it.
+     * 2. Incrementally upload a picture from the folder.
+     *
+     * Uncomment whichever mode you want to use and fill in the required information in changeAvatarStrings.
      */
 
     static WebDriver driver = new ChromeDriver();
     static WebDriverWait wait = new WebDriverWait(driver, 10);
-    static Random rand = new Random();
-    static File[] avatarArray = new File(changeAvatarStrings.pathToAvatars).listFiles();
-    static File avatarName = avatarArray[rand.nextInt(avatarArray.length)];
-    static String pickAvatar = avatarName.toString();
 
-    public static void main (String[] args) throws InterruptedException {
-
-        changeAvatar();
-
+    public static void main (String[] args) {
+        try {
+            //changeAvatarModes.randomChoice();
+            //changeAvatarModes.incrementalChoice();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            driver.quit();
+        }
     }
-
-    public static void changeAvatar() throws InterruptedException {
-
-        driver.navigate().to("https://discordapp.com/");
-
-        changeAvatarSteps.loginScreen();
-        changeAvatarSteps.enterInformation();
-        changeAvatarSteps.loginToAccount();
-        changeAvatarSteps.userSettings();
-        changeAvatarSteps.editAccount();
-        changeAvatarSteps.changeAvatar();
-
-        driver.quit();
-
-    }
-
 }
 
