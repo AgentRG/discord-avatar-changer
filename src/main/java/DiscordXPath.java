dc90eb6c1104bbfec7ae0f91b5f5c160f0a6579a
Rostyslav Gordin <ron.gordin@gmail.com>
2018-07-19 14:43:31 -0700

    Added a space after the = for optionsContainer

--- src/main/java/DiscordXPath.java
+++ src/main/java/DiscordXPath.java
@@ -10,7 +10,7 @@ public class DiscordXPath extends Main {
     static String loginToAccount = "//*[@type='submit']";
     static String loadingContainer = "//*[@class='container-16j22k']";
     static String optionsButton = "//button[@type='button' and contains(@class, 'button-2b6hmh')]";
-    static String optionsContainer ="//*[@class='layers-3iHuyZ flex-vertical flex-spacer']";
+    static String optionsContainer = "//*[@class='layers-3iHuyZ flex-vertical flex-spacer']";
     static String editProfile = "//button[contains(@class, 'userInfoViewingButton')]";
     static String uploadButton = "//input[@class='file-input']";
     static String saveButton = "//button[contains(@class, 'Green')]";
